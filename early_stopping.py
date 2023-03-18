89cebb9b3a2ab2139498435abdc4fbc58f609818
Maximilian Purrucker <m.purrucker@tu-braunschweig.de>
2023-03-18 18:39:46 +0100

    Bug fix after testing and new debug prints

--- early_stopping.py
+++ early_stopping.py
@@ -1,15 +1,17 @@
 import numpy as np
+import torch
 
 # Python class to evaluate your training result of a neural network and to avoid overfitting
 class EarlyStopping:
     # Initialize data
-    def __init__(self, lookback, deg):
+    def __init__(self, lookback, deg, debug=False):
         self.states = list()
         self.train_losses = list()
         self.val_losses = list()
         self.lookback = lookback
         self.deg = deg
         self.final_model = None
+        self.debug = debug
 
     # Check if overfitting is happening and safe best model state
     # Return True if overfitting started
@@ -17,15 +19,23 @@ class EarlyStopping:
         # Log current losses 
         self.train_losses.append(train_loss)
         self.val_losses.append(val_loss)
+        
+        self.log(f'Training loss: {train_loss}, validation loss: {val_loss}')
+
+        if len(self.train_losses) < 2: 
+            self.log("Skip early stop due to length of input array")
+            self.states.append(None)
+            return False
 
         # Define array that will be used to calculate the increasing/decreasing tendency of the data
-        train_loss_window = self.train_losses[:-min(self.lookback, len(self.train_losses))]
-        val_loss_window = self.val_losses[:-min(self.lookback, len(self.val_losses))]
+        train_loss_window = self.train_losses[len(self.train_losses)-min(self.lookback, len(self.train_losses)):]
+        val_loss_window = self.val_losses[len(self.val_losses)-min(self.lookback, len(self.val_losses)):]
 
         # Check if overfitting starts
         if self.is_decreasing(train_loss_window) and self.is_increasing(val_loss_window):
             # Overfitting starts
-            min_id = self.val_losses.index(min(self.val_losses))
+            self.log("Overfitting detected")
+            min_id = self.val_losses.index(min(self.val_losses[1:]))
             self.final_model = self.states[min_id]
             return True
         else:
@@ -36,7 +46,7 @@ class EarlyStopping:
     # Calculate slope of interpolated data with a range defined in lookback
     # Return slope
     def get_slope(self, data):
-        indices = [range(1, len(data) + 1)]
+        indices = list(range(1, len(data) + 1))
         return np.polyfit(indices, data, self.deg)[-2]
 
     # Check if slope is increasing
@@ -49,6 +59,21 @@ class EarlyStopping:
     def is_decreasing(self, data):
         return self.get_slope(data) < 0
     
+    # Save best model state 
+    def save_state(self, path):
+        self.log("Save state")
+        torch.save(self.final_model, path)
 
-        
+    # Get minimum training loss
+    def get_min_train_loss(self):
+        return min(self.train_losses)
+    
+    # Get minimum validation loss
+    def get_min_val_loss(self):
+        return min(self.val_losses)
+    
+    # Print debug information
+    def log(self, log):
+        if self.debug:
+            print(f'[DEBUG] : \tEpoche {len(self.train_losses)}\t {log}')
 
